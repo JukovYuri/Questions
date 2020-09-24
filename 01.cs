a0d4ff8de2fbfe2c22b0b4afbeb3235ce46c5f9d
JukovYuri <jukov_yuri@mail.ru>
2020-09-24 16:40:17 +0300

    Create 01.cs

---
+++ 01.cs
@@ -0,0 +1,15 @@
+﻿void OnTriggerEnter(Collider other)
+{
+	if (other.gameObject.name == "Player")
+	{
+		_target = other.gameObject;
+	}
+}
+
+void OnTriggerExit(Collider other)
+{
+	if (other.GetComponent<PlayerControllers>())
+	{
+		print(other.GetComponent<PlayerControllers>());
+	}
+}
